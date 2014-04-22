2b65cfd9db60a10c4c04e027158a270681e1e3a6
Sander Tolsma <github@tolsma.net>
2014-04-22 09:23:03 +0200

    Use different versions of node take 2

--- configs/default.js
+++ configs/default.js
@@ -188,9 +188,11 @@ var config = [
     {
         packagePath: "./cloud9.run.node",
         nodeVersions: {
-            "0.8" : "./node/node0.8",
-            "0.10": "./node/node0.10",
-            "0.11": "./node/node0.11"
+            "auto" : "./node/nodeAuto",
+            "0.8"  : "./node/node0.8",
+            "0.10" : "./node/node0.10",
+            "0.11" : "./node/node0.11",
+            "0.12" : "./node/node0.12"
             // and so on...
         },
         listenHint: "Important: in your scripts, use 'process.env.PORT' as port and '0.0.0.0' as host."
@@ -198,9 +200,11 @@ var config = [
     {
         packagePath: "./cloud9.run.node-debug",
         nodeVersions: {
-            "0.8" : "./node/node0.8",
-            "0.10": "./node/node0.10",
-            "0.11": "./node/node0.11"
+            "auto" : "./node/nodeAuto",
+            "0.8"  : "./node/node0.8",
+            "0.10" : "./node/node0.10",
+            "0.11" : "./node/node0.11",
+            "0.12" : "./node/node0.12"
             // and so on...
         },
         listenHint: "Important: in your scripts, use 'process.env.PORT' as port and '0.0.0.0' as host.",
